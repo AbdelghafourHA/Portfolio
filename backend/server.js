0cb44f12a8776a8c6d7bcd7c95b47da2a04f6a79
Hadjoudj Abdelghafour <ahmedhadjoudj303@gmail.com>
2026-04-01 20:19:12 +0100

    hello 7

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ connectDB();
 // Middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://abdelghafour.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://portfolio-six-beryl-14.vercel.app",
+    ],
   })
 );
 
